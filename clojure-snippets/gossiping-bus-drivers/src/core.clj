79203df05134d46946d9e43b5a1d259f0f1dab41
ayato-p <ayato-p@panchromatic.org>
2025-08-16 11:53:38 +0900

    impl bus stop function

--- clojure-snippets/gossiping-bus-drivers/src/core.clj
+++ clojure-snippets/gossiping-bus-drivers/src/core.clj
@@ -3,14 +3,19 @@
             [matcher-combinators.clj-test]))
 
 (defn stop [routes n]
-  (nth routes n))
+  {:pre [(<= 0 n)
+         (pos? (count routes))]}
+  (nth routes (mod n (count routes))))
 
 (defn simulate [drivers]
   (letfn []))
 
-
 (t/deftest stop-test
-  (t/is (= :x (stop [:x] 0))))
+  (t/are [expected routes n] (= expected (stop routes n))
+    :x [:x] 0
+    :y [:x :y] 1
+    :y [:x :y] 3
+    :x [:x :y] 4))
 
 (t/deftest simulate-test
   #_(t/is (match? :never
